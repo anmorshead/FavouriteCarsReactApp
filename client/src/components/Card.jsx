789d684cbe86be45b08f6e05fa2a1c609f1f7204
Andrea Morshead <andrea.morshead@gmail.com>
2024-11-19 14:01:21 -0400

    finished requirement 1

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const Card = (props) => {
+    return (
+        <div key={props._id}className="col-md-4">
+          <div className="card mb-4 box-shadow">
+            <img 
+              className="card-img-top" 
+              data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
+              alt="Thumbnail [100%x225]" 
+              style={{height: 225, width: '100%', display: 'block'}}
+              src={props.image}
+              data-holder-rendered="true" />
+            <div className="card-body">
+              <ul className="card-text">
+                <li>Generation: {props.generation}</li>
+                <li>{props.horsepower} horsepower</li>
+                <li>{props.torque} lb. -ft torque</li>
+                <li>Engine: {props.engine}</li>
+              </ul>
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="btn-group">
+                    <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                </div>
+                <p className="text-muted">{props.manufacturer} {props.model}</p>
+              </div>
+            </div>  
+          </div>
+        </div>
+      )
+}
+
+export default Card;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../css/main.css'
 import 'font-awesome/css/font-awesome.min.css';
 import axios from 'axios';
+import Card from './Card';
 
 const Main = () => {
   const [cars, setCars] = useState([])
@@ -33,38 +34,19 @@ const Main = () => {
         <div className="album py-5 bg-light">
           <div className="container">
             <div className="row">
-
-              {
-                cars.map(car => {
-                  return (
-                    <div key={car._id}className="col-md-4">
-                      <div className="card mb-4 box-shadow">
-                        <img 
-                          className="card-img-top" 
-                          data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
-                          alt="Thumbnail [100%x225]" 
-                          style={{height: 225, width: '100%', display: 'block'}}
-                          src={car.image}
-                          data-holder-rendered="true" />
-                        <div className="card-body">
-                          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <div className="d-flex justify-content-between align-items-center">
-                            <div className="btn-group">
-                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                                <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                            </div>
-                            <small className="text-muted">{car.model}</small>
-                          </div>
-                        </div>  
-                      </div>
-                    </div>
-                  )
-                })
-              }
-
-         
-
+              {cars.map(car => {
+                return <Card 
+                key={car._id} 
+                _id={car._id} 
+                image={car.image} 
+                model={car.model} 
+                manufacturer={car.manufacturer} 
+                generation={car.generation}
+                horsepower={car.performance.horsepower} 
+                engine={car.performance.engineType}
+                torque={car.performance.torque}
+                />
+              })}
             </div>
           </div>
         </div>
