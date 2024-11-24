7fef457f794741f03b77da53d70b445a438635d1
Andrea Morshead <andrea.morshead@gmail.com>
2024-11-24 16:28:23 -0400

    signin redirects to home page upon successful signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import Register from './components/Register';
 import SignIn from './components/SignIn';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -14,6 +15,7 @@ const App = () => {
       <div id="main-content">
         <Routes>
           <Route path="/" element={<Main />}/>
+          <Route path="/register" element={<Register/>}/>
           <Route path="/signin" element={<SignIn/>}/>
         </Routes>
       </div>
