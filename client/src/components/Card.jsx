2d1a74568045c4c776697acc96a41902af7b2c16
Andrea Morshead <andrea.morshead@gmail.com>
2024-12-03 08:44:34 -0400

    ready for marking-phase2

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -21,6 +21,11 @@ const Card = (props) => {
               </ul>
               <div className="d-flex justify-content-between align-items-center">
                 <p className="text-muted">{props.manufacturer} {props.model}</p>
+                <div className="btn-group">
+                    <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                </div>
               </div>
             </div>  
           </div>
